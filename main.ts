037098f2e4c7fcc91c9feca43f12997e42f6ff72
wcchun1234 <97726115+wcchun1234@users.noreply.github.com>
2024-09-24 08:34:47 +0800

    Create main.ts

---
+++ main.ts
@@ -0,0 +1,31 @@
+//% weight=100 color=#0fbc11 icon="\uf013" block="Motor Rotations"
+namespace motorRotations {
+    let startTime = 0
+    let motorRunning = false
+    let rotations = 0
+    let motorFullSpeedRPM = 120
+
+    //% block="start motor at speed %speed"
+    export function startMotor(speed: number): void {
+        rotations = 0
+        startTime = control.millis()
+        motorRunning = true
+        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, speed)
+    }
+
+    //% block="stop motor"
+    export function stopMotor(): void {
+        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 0)
+        motorRunning = false
+    }
+
+    //% block="show number of rotations"
+    export function showRotations(): void {
+        if (motorRunning) {
+            let elapsedSeconds = (control.millis() - startTime) / 1000
+            let rpm = motorFullSpeedRPM * (120 / 100)
+            rotations = (rpm / 60) * elapsedSeconds
+            basic.showNumber(Math.floor(rotations))
+        }
+    }
+}
